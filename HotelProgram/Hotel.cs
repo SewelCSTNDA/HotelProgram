1d81944afeb59fb517194a3db9d0f44b693d144a
m303user <m303user@M303-36>
2024-04-05 17:44:40 +0800

    Add project files.

---
+++ HotelProgram/Hotel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProgram
+{
+    public class Hotel
+    {
+        public string HotelName {  get; set; }
+        public string Location {  get; set; }
+        private List<HotelRoom> _allRooms;
+
+        public Hotel(string hotelName, string location, List<HotelRoom> rooms)
+        {
+            HotelName = hotelName;
+            Location = location;
+            _allRooms = rooms;
+        }
+
+        public Hotel()
+        {
+            _allRooms = new List<HotelRoom>();
+        }
+
+        public void addRoom(HotelRoom room)
+        {
+            _allRooms.Add(room);
+        }
+    }
+}
---
+++ HotelProgram/HotelManagementSystem.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProgram
+{
+    public class HotelManagementSystem
+    {
+        private List<Hotel> Hotels;
+        private List<User> Users;
+        private List<Reservation> Reservations;
+
+        public void RegisterUser(UserStringHandle user)
+        {
+
+        }
+
+        public void AddHotel(Hotel hotel)
+        {
+
+        }
+        public void DisplayHotels()
+        {
+
+        }
+        
+        public void BookReservation(Hotel hotel, HotelRoom room, Guest guest, DateTime startTime, DateTime endTime)
+        {
+
+        }
+
+        public void DisplayReservationDetails(int reservationNumber)
+        {
+
+        }
+    }
+}
